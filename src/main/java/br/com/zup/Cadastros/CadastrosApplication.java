040f2c8377620a245773d6f04185a0378051d4f9
Vinícius <vinicius.raymundo@zup.com.br>
2021-11-08 12:09:15 -0300

    Criar projeto

---
+++ src/main/java/br/com/zup/Cadastros/CadastrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.Cadastros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/Cadastros/CadastrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.Cadastros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
