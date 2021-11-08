800b85116f6c771064d302a4d6f97ad2ad01c90b
Vinícius <vinicius.raymundo@zup.com.br>
2021-11-08 12:14:12 -0300

    Criar classe cadastro

---
+++ src/main/java/br/com/zup/Cadastros/cadastro/Cadastro.java
@@ -0,0 +1,98 @@
+package br.com.zup.Cadastros.cadastro;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cadastros")
+public class Cadastro {
+    @Id
+    private String cpf;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private String sobrenome;
+    private String cidade;
+    private String bairro;
+    private String nomeDoParenteProximo;
+    private boolean moraSozinho;
+    private boolean temPet;
+    private int idade;
+
+    public Cadastro() {
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getNomeDoParenteProximo() {
+        return nomeDoParenteProximo;
+    }
+
+    public void setNomeDoParenteProximo(String nomeDoParenteProximo) {
+        this.nomeDoParenteProximo = nomeDoParenteProximo;
+    }
+
+    public boolean isMoraSozinho() {
+        return moraSozinho;
+    }
+
+    public void setMoraSozinho(boolean moraSozinho) {
+        this.moraSozinho = moraSozinho;
+    }
+
+    public boolean isTemPet() {
+        return temPet;
+    }
+
+    public void setTemPet(boolean temPet) {
+        this.temPet = temPet;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
