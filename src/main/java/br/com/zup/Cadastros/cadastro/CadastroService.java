b692b99555eeb0b10b59facd9056554b0d2e14af
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-08 14:51:31 -0300

    Criar lista de cadastros.

--- src/main/java/br/com/zup/Cadastros/cadastro/CadastroService.java
+++ src/main/java/br/com/zup/Cadastros/cadastro/CadastroService.java
@@ -3,8 +3,15 @@ package br.com.zup.Cadastros.cadastro;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class CadastroService {
     @Autowired
     private CadastroRepository cadastroRepository;
+
+    private List<Cadastro> cadastros = new ArrayList<>();
+
+
 }
