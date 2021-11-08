18c6751727b5127d19c2f12dbc937e72028f2916
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-08 15:43:03 -0300

    Adicionar Cadastros na lista de cadastros.

--- src/main/java/br/com/zup/Cadastros/cadastro/CadastroController.java
+++ src/main/java/br/com/zup/Cadastros/cadastro/CadastroController.java
@@ -1,6 +1,8 @@
 package br.com.zup.Cadastros.cadastro;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +14,14 @@ public class CadastroController {
 
     /*
     todo  1 - crie um metodo para cadastrar uma pessoa.
-      Para um cadastro todo os campos são obrigatórios EXCETO o campo dataDoCadastro que deve ser preenchido pelo proprio sistema e o client não deve saber da existencia desse campo
+    Para um cadastro todo os campos são obrigatórios EXCETO o campo dataDoCadastro que deve ser preenchido pelo proprio sistema e o client não deve saber da existencia desse campo
+    */
+    @PostMapping
+    public void adicionarCadastro(@RequestBody Cadastro cadastro){
+        cadastroService.salvarCadastros(cadastro);
+    }
+
+     /*
      todo 2 - Faça um metodo que retorna a lista inteira de cadastros ou filtrado por cadastros que moram sozinhos, que tem pet ou por idade.
      nessa lista deve ser retornado apenas os campos ID, NOME e SOBRENOME.
      todo 3 - faça um metodo para DELETAR um cadastro por id.
