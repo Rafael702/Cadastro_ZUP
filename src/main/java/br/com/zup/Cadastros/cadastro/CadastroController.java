3e870bb4bb37e64eb5920ccb91ce8f093f33bc81
Vinícius <vinicius.raymundo@zup.com.br>
2021-11-08 12:24:24 -0300

    Adicionar ToDos

--- src/main/java/br/com/zup/Cadastros/cadastro/CadastroController.java
+++ src/main/java/br/com/zup/Cadastros/cadastro/CadastroController.java
@@ -10,4 +10,13 @@ public class CadastroController {
     @Autowired
     private CadastroService cadastroService;
 
+    /*
+    todo  1 - crie um metodo para cadastrar uma pessoa.
+      Para um cadastro todo os campos são obrigatórios EXCETO o campo dataDoCadastro que deve ser preenchido pelo proprio sistema e o client não deve saber da existencia desse campo
+     todo 2 - Faça um metodo que retorna a lista inteira de cadastros ou filtrado por cadastros que moram sozinhos, que tem pet ou por idade.
+     nessa lista deve ser retornado apenas os campos ID, NOME e SOBRENOME.
+     todo 3 - faça um metodo para DELETAR um cadastro por id.
+     todo 4 - faça um metodo que retorna TODOS os dados de um usuario pesquisado pelo ID.
+     */
+
 }
