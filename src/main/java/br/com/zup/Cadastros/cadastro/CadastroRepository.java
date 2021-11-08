861d4b1931a6c7e59f2ea6416492805acdace021
Vinícius <vinicius.raymundo@zup.com.br>
2021-11-08 12:16:27 -0300

    Criar repository para cadastro

---
+++ src/main/java/br/com/zup/Cadastros/cadastro/CadastroRepository.java
@@ -0,0 +1,6 @@
+package br.com.zup.Cadastros.cadastro;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CadastroRepository extends CrudRepository<Cadastro, String> {
+}
