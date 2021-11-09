87638b8404151344bc2dbcc935d32e80059b7972
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-09 14:30:28 -0300

    Adicionar getters e setters.

--- src/main/java/br/com/zup/Cadastros/cadastro/dtos/CadastroDTO.java
+++ src/main/java/br/com/zup/Cadastros/cadastro/dtos/CadastroDTO.java
@@ -13,4 +13,76 @@ public class CadastroDTO {
 
     public CadastroDTO() {
     }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getNomeDoParenteProximo() {
+        return nomeDoParenteProximo;
+    }
+
+    public void setNomeDoParenteProximo(String nomeDoParenteProximo) {
+        this.nomeDoParenteProximo = nomeDoParenteProximo;
+    }
+
+    public boolean isMoraSozinho() {
+        return moraSozinho;
+    }
+
+    public void setMoraSozinho(boolean moraSozinho) {
+        this.moraSozinho = moraSozinho;
+    }
+
+    public boolean isTemPet() {
+        return temPet;
+    }
+
+    public void setTemPet(boolean temPet) {
+        this.temPet = temPet;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 }
