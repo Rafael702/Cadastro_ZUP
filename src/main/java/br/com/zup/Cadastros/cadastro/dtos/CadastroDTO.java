fc68ce1eeb34ce72463efbf146fe635a93be8c26
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-09 13:56:07 -0300

    Criar Classe CadastroDTO.

---
+++ src/main/java/br/com/zup/Cadastros/cadastro/dtos/CadastroDTO.java
@@ -0,0 +1,16 @@
+package br.com.zup.Cadastros.cadastro.dtos;
+
+public class CadastroDTO {
+    private String cpf;
+    private String nome;
+    private String sobrenome;
+    private String cidade;
+    private String bairro;
+    private String nomeDoParenteProximo;
+    private boolean moraSozinho;
+    private boolean temPet;
+    private int idade;
+
+    public CadastroDTO() {
+    }
+}
