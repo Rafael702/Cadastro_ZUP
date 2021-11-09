e27adc472dbe3712f7a84f063f6f5753bd92c097
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-09 14:01:07 -0300

    Acrescentar o set no atributo Data Do Cadastro para salvar Cadastros.

--- src/main/java/br/com/zup/Cadastros/cadastro/CadastroService.java
+++ src/main/java/br/com/zup/Cadastros/cadastro/CadastroService.java
@@ -3,16 +3,25 @@ package br.com.zup.Cadastros.cadastro;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDate;
 
 @Service
 public class CadastroService {
     @Autowired
     private CadastroRepository cadastroRepository;
 
-    public void salvarCadastros(Cadastro cadastro){
+    public void salvarCadastros(Cadastro cadastro) {
+        cadastro.setDataDoCadastro(LocalDate.now());
+
         cadastroRepository.save(cadastro);
     }
+
+//    public boolean validaCadastro(){
+//        Cadastro cadastro;
+//
+//        if(!cadastro.getCpf().isEmpty() & !cadastro.)
+//
+//    }
+
 
 }
